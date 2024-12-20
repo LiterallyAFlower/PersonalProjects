0a67735bb3ea4e46c7b8b44215434e37b55b8836
Marigold <111760235+LiterallyAFlower@users.noreply.github.com>
2024-12-20 17:32:30 -0500

    Altered thing, line breaks

--- TicTacToe/board.java
+++ TicTacToe/board.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class board
 {
@@ -14,18 +14,18 @@ public class board
         }
     }
 
-    static void toString()
+    @Override
+    public String toString()
     {
-        System.out.println("|   |   |   |");
-        System.out.println("| "+spaces[0]+" | "
-        +spaces[1]+" | "+spaces[2]+" |");
-        System.out.println("|-----------|");
-        System.out.println("| "+spaces[3]+" | "
-        +spaces[4]+" | "+spaces[5]+" |");
-        System.out.println("|-----------|");
-        System.out.println("| "+spaces[6]+" | "
-        +spaces[7]+" | "+spaces[8]+" |");
-        System.out.println("|---|---|---|");
+        String s = "";
+        s+="|   |   |   |";
+        s+="| "+spaces[0]+" | "+spaces[1]+" | "+spaces[2]+" |";
+        s+="|-----------|";
+        s+="| "+spaces[3]+" | "+spaces[4]+" | "+spaces[5]+" |";
+        s+="|-----------|";
+        s+="| "+spaces[6]+" | "+spaces[7]+" | "+spaces[8]+" |";
+        s+="|---|---|---|";
+        return s;
     }
     
     static String checkWin()
@@ -33,30 +33,14 @@ public class board
         for (int a = 0; a < 8; a++) {
             String line = null;
             switch (a) {
-                case 0:
-                    line = spaces[0] + spaces[1] + spaces[2];
-                    break;
-                case 1:
-                    line = spaces[3] + spaces[4] + spaces[5];
-                    break;
-                case 2:
-                    line = spaces[6] + spaces[7] + spaces[8];
-                    break;
-                case 3:
-                    line = spaces[0] + spaces[3] + spaces[6];
-                    break;
-                case 4:
-                    line = spaces[1] + spaces[4] + spaces[7];
-                    break;
-                case 5:
-                    line = spaces[2] + spaces[5] + spaces[8];
-                    break;
-                case 6:
-                    line = spaces[0] + spaces[4] + spaces[8];
-                    break;
-                case 7:
-                    line = spaces[2] + spaces[4] + spaces[6];
-                    break;
+                case 0 -> line = spaces[0] + spaces[1] + spaces[2];
+                case 1 -> line = spaces[3] + spaces[4] + spaces[5];
+                case 2 -> line = spaces[6] + spaces[7] + spaces[8];
+                case 3 -> line = spaces[0] + spaces[3] + spaces[6];
+                case 4 -> line = spaces[1] + spaces[4] + spaces[7];
+                case 5 -> line = spaces[2] + spaces[5] + spaces[8];
+                case 6 -> line = spaces[0] + spaces[4] + spaces[8];
+                case 7 -> line = spaces[2] + spaces[4] + spaces[6];
             } //X winner
             if (line.equals("XXX")) {
                 return "X";
@@ -71,11 +55,15 @@ public class board
             if (Arrays.asList(spaces).contains(String.valueOf(a+1))) {
                 break;
             }
-            else if (a == 8) return "draw";
+            else if (a == 8) 
+            {
+                return "draw";
+            }
         }
+        System.out.println(
+        turn + "'s turn; enter a slot number to place " + turn + " in:");
+        return null;
     }
     // To enter X or O at the desired location on board
-    System.out.println(
-        turn + "'s turn; enter a slot number to place " + turn + " in:");
-    return null;
+    
 }
