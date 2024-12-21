b3b144c783bb824fd6541d63badf2e064a4cbe83
LiterallyAFlower <jjp8541@rit.edu>
2024-12-21 10:50:22 -0500

    finished basic utility, going to add a New Game loop, and start on a html/js port.

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -1,14 +1,56 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        board b = new board();
-        board.turn = "X";
-        String winner = null;
+    
+    
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            board b = new board();
+            board.turn = "X";
+            String winner = null;
+    
+    
+            //Start of loop
+            System.out.println("Welcome to TicTacToe!");
+            System.out.println(b.toString());
+            System.out.println("X will play first. Please select a number to make a move!");
+    
+    
+            while(winner==null) 
+            {
+                int input;
+    
+                //number 1-9
+                try {
+                    input = in.nextInt();
+                    if(input<1 || input > 9){
+                        System.out.println("Invalid input, enter a number from 1-9.");
+                        continue;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input, enter a number from 1-9.");
+                    continue;
+                }
+                if(board.spaces[input-1].equals(String.valueOf(input))) 
+                {
+                    board.spaces[input-1] = board.turn;
+                    if(board.turn.equals("X")) board.turn = "O";
+                    else board.turn = "X";
 
+                    System.out.println(b.toString());
+                    winner = board.checkWin();
+            }
+            else {
+                System.out.println("Slot already taken! Choose a new number.");
+            }
+        }
 
-        //
-        b.toString();
+        if(winner.equalsIgnoreCase("draw")){
+            System.out.println("It's a draw! Thanks for playing.");
+        } else {
+            System.out.println("Congratulations! "+winner+"'s have won! Thanks for playing.");
+        }
+        in.close();
     }
 }
--- TicTacToe/board.java
+++ TicTacToe/board.java
@@ -3,43 +3,55 @@ import java.util.*;
 public class board
 {
     static String[] spaces;
-    static String turn;
-
-    public board() 
-    {
-        spaces = new String[9];
-        turn = "X";
-        for (int i = 0; i < 9; i++) {
-            spaces[i] = String.valueOf(i + 1);
-        }
-    }
-
-    @Override
-    public String toString()
-    {
-        String s = "";
-        s+="|   |   |   |";
-        s+="| "+spaces[0]+" | "+spaces[1]+" | "+spaces[2]+" |";
-        s+="|-----------|";
-        s+="| "+spaces[3]+" | "+spaces[4]+" | "+spaces[5]+" |";
-        s+="|-----------|";
-        s+="| "+spaces[6]+" | "+spaces[7]+" | "+spaces[8]+" |";
-        s+="|---|---|---|";
-        return s;
-    }
+        static String turn;
     
-    static String checkWin()
-    {
-        for (int a = 0; a < 8; a++) {
-            String line = null;
-            switch (a) {
-                case 0 -> line = spaces[0] + spaces[1] + spaces[2];
-                case 1 -> line = spaces[3] + spaces[4] + spaces[5];
-                case 2 -> line = spaces[6] + spaces[7] + spaces[8];
-                case 3 -> line = spaces[0] + spaces[3] + spaces[6];
-                case 4 -> line = spaces[1] + spaces[4] + spaces[7];
-                case 5 -> line = spaces[2] + spaces[5] + spaces[8];
-                case 6 -> line = spaces[0] + spaces[4] + spaces[8];
+        public board() 
+        {
+            spaces = new String[9];
+            turn = "X";
+            for (int i = 0; i < 9; i++) {
+                spaces[i] = String.valueOf(i + 1);
+            }
+        }
+    
+        public int makeMove(String player, int space)
+        {
+            if(spaces[space]=="X"||spaces[space]=="O")
+            {
+                System.out.println("That space has already been taken! Choose a new space: \n");
+                return 0;
+            } else {
+                spaces[space] = player;
+                return 1;
+            }
+        }
+    
+        @Override
+        public String toString()
+        {
+            String s = "";
+            s+="|---|---|---|\n";
+            s+="| "+spaces[0]+" | "+spaces[1]+" | "+spaces[2]+" |\n";
+            s+="|-----------|\n";
+            s+="| "+spaces[3]+" | "+spaces[4]+" | "+spaces[5]+" |\n";
+            s+="|-----------|\n";
+            s+="| "+spaces[6]+" | "+spaces[7]+" | "+spaces[8]+" |\n";
+            s+="|---|---|---|\n";
+            return s;
+        }
+        
+        static String checkWin()
+        {
+            for (int a = 0; a < 8; a++) {
+                String line = null;
+                switch (a) {
+                    case 0 -> line = spaces[0] + spaces[1] + spaces[2];
+                    case 1 -> line = spaces[3] + spaces[4] + spaces[5];
+                    case 2 -> line = spaces[6] + spaces[7] + spaces[8];
+                    case 3 -> line = spaces[0] + spaces[3] + spaces[6];
+                    case 4 -> line = spaces[1] + spaces[4] + spaces[7];
+                    case 5 -> line = spaces[2] + spaces[5] + spaces[8];
+                    case 6 -> line = spaces[0] + spaces[4] + spaces[8];
                 case 7 -> line = spaces[2] + spaces[4] + spaces[6];
             } //X winner
             if (line.equals("XXX")) {
